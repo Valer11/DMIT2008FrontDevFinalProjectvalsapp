e8750eed61b11e22aedcec1bf5f0977464e598a9
Valer11 <Valer11@users.noreply.github.com>
2023-11-29 16:04:04 -0700

    Update page.js
    image fixed

--- app/bike/[id]/page.js
+++ app/bike/[id]/page.js
@@ -1,6 +1,6 @@
-// Import necessary components and functions
 import { getBikes } from "@/lib/firebase/getBikes";
 import { PageHeader } from "@/components/header/PageHeader";
+import Image from "next/image";
 
 // Function to fetch a single bike based on ID
 async function getBike(id) {
@@ -20,7 +20,6 @@ export default async function BikePage({ params }) {
 
   // Check if the bikeData exists
   if (!bikeData) {
-    // You might want to handle the case where the bike with the given ID is not found
     return <p>Bike not found</p>;
   }
 
@@ -31,16 +30,29 @@ export default async function BikePage({ params }) {
       <main className="min-h-screen py-24">
         <div className="max-w-screen-lg m-auto">
           {/* Display individual bike data */}
-          <h2 className="text-gray-600 font-semibold">
-            Bike Name: {bikeData.productName}
-          </h2>
-          <h2 className="text-gray-600 font-semibold">
-            Bike Price: {bikeData.productPrice}
-          </h2>
-          <h2 className="text-gray-600 font-semibold">
-            Bike Description: {bikeData.shortDescription}
-          </h2>
-          {/* Add other bike details as needed */}
+          <div className="ml-44 mb-10">
+            <aside className="md:flex bg-slate-100">
+              <Image
+                src={`/${bikeData.productImage}`}
+                width={380}
+                height={380}
+                alt={bikeData.productName}
+              />
+
+              <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+                <h2 className="text-gray-600 font-semibold">
+                  Bike Name: {bikeData.productName}
+                </h2>
+                <h2 className="text-gray-600 font-semibold">
+                  Bike Price: {bikeData.productPrice}
+                </h2>
+                <h2 className="text-gray-600 font-semibold">
+                  Bike Description: {bikeData.shortDescription}
+                </h2>
+                {/* Add other bike details as needed */}
+              </div>
+            </aside>
+          </div>
         </div>
       </main>
     </>
